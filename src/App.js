b61a8669d00c87101418a69c6aeb6de827ca5828
hdarparish <Hosseindarp@gmail.com>
2021-08-29 18:01:37 -0400

    add navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
-
+import React from "react";
+//pages & components
+import Home from "./pages/Home";
+import Nav from "./components/Nav";
+//styles
+import GlobalStyles from "./components/GlobalStyles";
+import styled from "styled-components";
 
 function App() {
   return (
-    <div className="App">
-    </div>
+    <ReactApp>
+      <GlobalStyles />
+      <Nav />
+      <Home />
+    </ReactApp>
   );
 }
+
+const ReactApp = styled.div``;
 
 export default App;
---
+++ src/components/GlobalStyles.js
@@ -0,0 +1,47 @@
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyles = createGlobalStyle`
+*{
+    margin:0;
+    padding:0;
+    box-sizing: border-box;
+}
+body{    
+    font-family: 'Montserrat' , sans-serif;
+    width:100%;
+    /*       background-color: #f5eeee; */
+  /*   background:#141414; */
+}
+h2{
+    font-size:3rem;
+    font-family: 'Montserrat' , sans-serif;
+    font-weight:lighter;
+    color:#333;
+}
+h3{
+    font-size:1.3rem;
+    color: #333;
+    padding:1.5rem 0rem;
+}
+p{
+    font-size:1.5rem;
+    line-height:200%;
+    color:#696969;
+}
+span{
+    font-weight: bold;
+    color: #39af95;
+}
+a{
+    text-decoration:none;
+    color: #333;
+}
+img{
+    display:block;
+}
+input, textarea{
+    font-family: 'Montserrat' , sans-serif;
+}
+`;
+
+export default GlobalStyles;
