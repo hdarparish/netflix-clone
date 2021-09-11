9bdffaa0b4f324936ba6ee3d203d0f358a759f44
hdarparish <Hosseindarp@gmail.com>
2021-09-11 17:20:44 -0400

    add movie swiper

--- src/actions/movieAction.js
+++ src/actions/movieAction.js
@@ -10,21 +10,21 @@ import {
 
 export const loadMovies = () => async (dispatch) => {
   const actionMoviesList = await axios.get(actionMoviesURL());
-  const animationMoviesList = await axios.get(animationMoviesURL());
+  /*   const animationMoviesList = await axios.get(animationMoviesURL());
   const HorrorMoviesList = await axios.get(horrorMoviesURL());
   const romanceMoviesList = await axios.get(romanceMoviesURL());
   const tvMoviesList = await axios.get(tvMoviesURL());
-  const popularTvList = await axios.get(popularTvURL());
+  const popularTvList = await axios.get(popularTvURL()); */
 
   dispatch({
     type: "FETCH_MOVIES",
     payload: {
       actionMovies: actionMoviesList.data.results,
-      animationMovies: animationMoviesList.data.results,
+      /*       animationMovies: animationMoviesList.data.results,
       horrorMovies: HorrorMoviesList.data.results,
       romanceMovies: romanceMoviesList.data.results,
       tvMovies: tvMoviesList.data.results,
-      popularTv: popularTvList.data.results,
+      popularTv: popularTvList.data.results, */
     },
   });
 };
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,11 +4,23 @@ import MovieCard from "../components/MovieCard";
 import MovieDetail from "../components/MovieDetail";
 //styles
 import { motion, AnimatePresence, AnimateSharedLayout } from "framer-motion";
+import { Swiper, SwiperSlide } from "swiper/react";
+import SwiperCore, {
+  Navigation,
+  Pagination,
+  Mousewheel,
+  Keyboard,
+} from "swiper";
+import "swiper/swiper.scss";
+import "swiper/components/navigation/navigation.scss";
 //redux
 import { useDispatch, useSelector } from "react-redux";
 import { loadMovies } from "../actions/movieAction";
 //router
 import { useLocation } from "react-router-dom";
+
+// install Swiper modules
+SwiperCore.use([Navigation]);
 
 const Home = () => {
   const location = useLocation();
@@ -36,13 +48,22 @@ const Home = () => {
         </AnimatePresence>
         <motion.div className="genre-section">
           <motion.h3>Action</motion.h3>
-          <motion.div className="movie-wrapper">
-            {actionMovies.map((movie) => (
-              <MovieCard movie={movie} key={movie.id} />
-            ))}
-          </motion.div>
+          <Swiper
+            slidesPerView={6}
+            slidesPerGroup={5}
+            className="mySwiper"
+            navigation
+          >
+            <motion.div className="movie-wrapper">
+              {actionMovies.map((movie) => (
+                <SwiperSlide>
+                  <MovieCard movie={movie} key={movie.id} />
+                </SwiperSlide>
+              ))}
+            </motion.div>
+          </Swiper>
         </motion.div>
-        <motion.div className="genre-section">
+        {/* <motion.div className="genre-section">
           <motion.h3>Animation</motion.h3>
           <motion.div className="movie-wrapper">
             {animationMovies.map((movie) => (
@@ -81,7 +102,7 @@ const Home = () => {
               <MovieCard movie={movie} key={movie.id} />
             ))}
           </motion.div>
-        </motion.div>
+        </motion.div> */}
       </AnimateSharedLayout>
     </motion.div>
   );
