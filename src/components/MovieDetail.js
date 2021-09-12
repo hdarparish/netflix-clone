b16154ff9995b2793e49ad9980d4477591054d05
hdarparish <Hosseindarp@gmail.com>
2021-09-12 19:45:26 -0400

    show movie year and runtime

--- src/components/MovieDetail.js
+++ src/components/MovieDetail.js
@@ -16,9 +16,15 @@ const MovieDetail = ({ pathId }) => {
       history.push("/");
     }
   };
+  const timeConvert = (totalMinutes) => {
+    let hours = Math.floor(totalMinutes / 60);
+    let minutes = totalMinutes % 60;
+    return `${hours}h ${minutes}m`;
+  };
   //Data
   const { movieDetail, isLoading } = useSelector((state) => state.detail);
-
+  const movieYear = new Date(movieDetail.release_date).getFullYear();
+  const movieTime = timeConvert(movieDetail.runtime);
   return (
     <>
       {!isLoading && (
@@ -29,11 +35,17 @@ const MovieDetail = ({ pathId }) => {
               alt={`${movieDetail.title} poster`}
             />
             <h3>{movieDetail.title}</h3>
+            <div className="movie-year-time">
+              <h4>{movieYear}</h4>
+              <h4>{movieTime}</h4>
+            </div>
+
             <motion.div className="movie-detail">
               <p>{movieDetail.overview}</p>
               <div className="movie-genre">
                 <p>
-                  Genre:
+                  <span>Genre:</span>
+
                   {movieDetail.genres &&
                     movieDetail.genres.map((genre) => genre.name).join(", ")}
                 </p>
